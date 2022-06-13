d02a90399fc57a10fd55be946c3c35eee00cb96d
胡文凡 <wenfan.hu@infaith.com.cn>
2022-06-13 19:08:09 +0800

    initial

---
+++ app/src/main/java/com/example/testscroll/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.testscroll
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun adjust(dx: Int = 0, dy: Int = 0) {
+
+    }
+}
